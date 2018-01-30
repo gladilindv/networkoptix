ee8418be495af6eafd79617c592d73e6cfb8bb21
gladilindv <gladilindv@users.noreply.github.com>
2018-01-30 12:28:24 +0300

    add ctor (default, copy, move)

--- c++/syncqueue.cpp
+++ c++/syncqueue.cpp
@@ -10,14 +10,24 @@ std::mutex g_lockprint;
 
 
 template <typename T>
-class SyncQueue {
+class SyncQueue final {
 	mutable std::mutex m;
 	std::queue<T> q;
 	std::condition_variable c;
-
-	static int cnt;
+	
+	// count pop events with empty queue
+	static unsigned int countWait;
+	
 public:
-	T pop(){
+	explicit SyncQueue() = default;
+	~SyncQueue() noexcept = default;
+	
+	SyncQueue(const SyncQueue&) = delete;
+	SyncQueue(SyncQueue&&) = delete;
+	SyncQueue& operator=(const SyncQueue&) = delete;
+	SyncQueue& operator=(SyncQueue&&) = delete;
+	
+	T pop() noexcept {
 		// try to lock main mutex
 		std::unique_lock<std::mutex> locker(m);
 		
@@ -25,7 +35,7 @@ public:
 		if(q.empty()){
 			{
 				std::lock_guard<std::mutex> lp(g_lockprint);
-				std::cout << "\t\tempty  " << ++cnt << " ...waiting [" << std::this_thread::get_id() << "]" << std::endl;
+				std::cout << "\t\tempty  " << ++countWait << " ...waiting [" << std::this_thread::get_id() << "]" << std::endl;
 			}
 			
 			// wait notification // block spurious wakeup
@@ -44,7 +54,7 @@ public:
 		return e;
 	}
 
-	void push(const T& e){
+	void push(const T& e) noexcept {
 		// try to lock main mutex
 		std::unique_lock<std::mutex> locker(m);
 
@@ -58,14 +68,14 @@ public:
 		}
 	}
 
-	int count() const {
+	int count() const noexcept {
 		std::lock_guard<std::mutex> locker(m);
 		return q.size();
 	}
 };
 
 template <typename T>
-int SyncQueue<T>::cnt = 0;
+unsigned int SyncQueue<T>::countWait = 0;
 
 
 
@@ -75,7 +85,7 @@ void testPush(SyncQueue<int>& q, int aCnt){
 		std::cout << "entered thread " << std::this_thread::get_id() << std::endl;
 	}
 	std::mt19937 gen(time(0)); 
-    std::uniform_int_distribution<> uid(100, 200);
+	std::uniform_int_distribution<> uid(100, 200);
 	for (int i = 0; i < aCnt; i++){
 		q.push(i);
 		std::this_thread::sleep_for(std::chrono::milliseconds(uid(gen)));
@@ -89,7 +99,7 @@ void testPop(SyncQueue<int>& q, int aCnt){
 		std::cout << "entered thread " << std::this_thread::get_id() << std::endl;
 	}
 	std::mt19937 gen(time(0)); 
-    std::uniform_int_distribution<> uid(100, 400);
+	std::uniform_int_distribution<> uid(100, 400);
 	for (int i = 0; i < aCnt; i++){
 		q.pop();
 		std::this_thread::sleep_for(std::chrono::milliseconds(uid(gen)));
@@ -104,13 +114,14 @@ int main(){
 	std::cout << "start with " << q.count() << std::endl;
 
 	std::thread t1(testPush, std::ref(q), 100);
-    std::thread t2(testPop, std::ref(q), 50);
-    std::thread t3(testPop, std::ref(q), 50);
-    t1.join();
-    t2.join();
-    t3.join();
+	std::thread t2(testPop, std::ref(q), 50);
+	std::thread t3(testPop, std::ref(q), 50);
+	
+	t1.join();
+	t2.join();
+	t3.join();
 
-    std::cout << "end with " << q.count() << std::endl;
+	std::cout << "end with " << q.count() << std::endl;
 
 	return 0;
 }
